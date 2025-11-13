020bfde45e192ba8c4d961f6a439bccf3448b13e
mariamorgana <84101707+mariamorgana@users.noreply.github.com>
2025-11-13 10:30:52 -0300

    número ao quadrado

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title('Meu primeiro app😍')
+
+st.header('Vamos fazer algo vom interatividade')
+
+n = st.number_input('Entre com seu número')
+
+st.write(f'O número que você escolheu ao quadrado é {n**2}.')
